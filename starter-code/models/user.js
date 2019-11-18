a6696d78a5e2cdb375b7d06ebf946970bb72d4d9
another-dj <alexis_forestier@hotmail.com>
2019-11-18 20:06:07 +0000

    having troubles with the update part

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,3 +1,22 @@
-'use strict';
+"use strict";
 
-// User model goes here
+const mongoose = require("mongoose");
+//const Schema = mongoose.schema;
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  hashpassword: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  }
+});
+
+const user = mongoose.model("User", userSchema);
+module.exports = user;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,10 +1,134 @@
-'use strict';
+"use strict";
 
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
+const User = require("../models/user"); // the model
+const bcrypt = require("bcryptjs");
+const bcryptSalt = 10;
 
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Hello World!' });
+const routeGuard = require("../middleware/route-guard"); // route guards
+
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Hello World!" });
 });
+
+router.get("/signup", (req, res, next) => {
+  // go to signup
+  res.render("signup");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+router.post("/signup", (req, res, next) => {
+  // create a user
+  const username = req.body.username;
+  const password = req.body.password;
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+
+  User.create({
+    username,
+    hashpassword: hashPass
+  })
+    .then(createdUser => {
+      console.log(createdUser);
+      res.redirect("/");
+    })
+    .catch(error => {
+      console.log(error);
+      res.render(error);
+    });
+});
+
+// logging in
+router.post("/login", (req, res, next) => {
+  const theUsername = req.body.username;
+  const thePassword = req.body.password;
+  let userId;
+
+  if (theUsername === "" || thePassword === "") {
+    res.render("login", {
+      error: "Please enter both, username and password to sign up."
+    });
+    return;
+  }
+
+  User.findOne({ username: theUsername })
+    .then(user => {
+      if (!user) {
+        return Promise.reject(new Error("The username doesn't exist."));
+      } else {
+        userId = user._id;
+        return bcrypt.compareSync(thePassword, user.hashpassword);
+      }
+    })
+    .then(match => {
+      if (match) {
+        req.session.user = userId;
+        res.redirect("/main");
+      } else {
+        res.render("login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+/* if (bcrypt.compareSync(thePassword, user.hashpassword)) {
+        console.log(thePassword, user.hashpassword);
+        // Save the login in the session!
+        req.session.user = user;
+        res.redirect("/");
+      } else {
+        res.render("login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+    })
+    .catch(error => {
+      console.log("------------------------------------->", error);
+      next(error);
+    });
+}); */
+
+// Protected routes
+
+/* router.get("/main", (req, res, next) => {
+  
+  res.render("main");
+});
+
+router.get("/private", (req, res, next) => {
+  
+  res.render("private");
+}); */
+
+// Private Page
+// Set a controller for the private page,
+// preceded by the middleware that prevents unauthenticated users to visit
+router.get("/private", routeGuard, (req, res, next) => {
+  res.render("private");
+});
+
+router.get("/main", routeGuard, (req, res, next) => {
+  res.render("main");
+});
+
+router.get("/profile", routeGuard, (req, res, next) => {
+  res.render("profile");
+});
+
+router.get("/edit", routeGuard, (req, res, next) => {
+  res.render("edit");
+});
+
+
+
+
 
 module.exports = router;
